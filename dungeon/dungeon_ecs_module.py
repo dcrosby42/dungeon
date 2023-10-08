0ddd298fdd15b8af224d8812937b4b1ac9928576
David Crosby <dcrosby42@gmail.com>
2023-10-08 22:39:00 +0000

    Factoring out DungeonRenderer

--- dungeon/dungeon_ecs_module.py
+++ dungeon/dungeon_ecs_module.py
@@ -354,74 +354,7 @@ class DungeonModule(Module[DungeonState]):
         return state
 
     def draw(self, state: DungeonState, output: Output):
-        self.draw_ui(state, output)
-
-        player_e = next(
-            (
-                e
-                for e in state.estore.select(Player)
-                if e.get(Player).player_id == state.my_player_id
-            )
-        )
-        room_id = player_e.get(Room).room_id
-
-        with output.offset(Pos(1, 1)):  # offset to be within the UI borders
-            for ent in sorted(
-                state.estore.select(Drawable, Loc, Room),
-                key=lambda e: e.get(Drawable).layer,
-            ):
-                # for ent in state.estore.select(Drawable, Loc, Room):
-                if ent.get(Room).room_id == room_id:
-                    output.print_at(ent.get(Loc).to_pos(), ent.get(Text).text)
-
-    def draw_ui(self, state: DungeonState, output: Output):
-        """render bound box and labels"""
-        width = ROOM_WIDTH + 2
-        height = ROOM_HEIGHT
-
-        # messages
-        output.print_at(
-            Pos(0, height + 3),
-            output.term.darkgray
-            + "\n".join(list(reversed(state.messages))[0:5])
-            + output.term.normal,
-        )
-
-        # for e in state.estore.select(Controller):
-        #     con = e.get(Controller)
-        #     output.print_at(
-        #         Pos(0, height + 3), output.term.blue + repr(con) + output.term.normal
-        #     )
-
-        # bounds
-        hbar = "+" + ("-" * (width - 2)) + "+"
-        bounds = hbar + "\n"
-        for y in range(0, height):
-            bounds += "|" + (" " * (width - 2)) + "|\n"
-        bounds += hbar
-        output.print_at(Pos(0, 0), bounds)
-
-        # status
-        # with output.offset(Pos(0, height + 1)):
-        #     item_str = ""
-        #     item = self.last_item_at(state, state.player.pos)
-        #     if item:
-        #         item_str = f" {item.name} "
-        #     obst_str = ""
-        #     obst = self.obstacle_at(state, state.player.pos)
-        #     if obst:
-        #         obst_str = f" >> {obst.name} "
-        #     output.print_at(
-        #         Pos(2, 0),
-        #         f"({state.player.pos.x},{state.player.pos.y}){item_str}{obst_str}",
-        #     )
-
-        # inventory
-        # with output.offset(Pos(0, height + 2)):
-        #     output.print_at(
-        #         Pos(0, 0),
-        #         f"{output.term.normal}Gear: {output.term.gold_on_black}{', '.join([i.name for i in state.player.items])}{output.term.normal}",
-        #     )
+        DungeonRenderer(state, output).draw()
 
     def _init_entity_store(self):
         estore = EntityStore()
@@ -499,3 +432,83 @@ class DungeonModule(Module[DungeonState]):
         door.add(Loc(x=4, y=0))
         door.add(Text(text="#"))
         door.add(Drawable())
+
+
+class DungeonRenderer:
+    def __init__(self, state, output):
+        self.state = state
+        self.output = output
+        self.estore = state.estore
+        self.player_id = state.my_player_id
+
+        self.player_ent = next(
+            (
+                e
+                for e in self.state.estore.select(Player)
+                if e.get(Player).player_id == self.player_id
+            )
+        )
+
+    def draw(self):
+        self.draw_ui()
+
+        room_id = self.player_ent.get(Room).room_id
+
+        with self.output.offset(Pos(1, 1)):  # offset to be within the UI borders
+            for ent in sorted(
+                self.state.estore.select(Drawable, Loc, Room),
+                key=lambda e: e.get(Drawable).layer,
+            ):
+                # for ent in state.estore.select(Drawable, Loc, Room):
+                if ent.get(Room).room_id == room_id:
+                    self.output.print_at(ent.get(Loc).to_pos(), ent.get(Text).text)
+
+    def draw_ui(self):
+        """render bound box and labels"""
+        width = ROOM_WIDTH + 2
+        height = ROOM_HEIGHT
+
+        # messages
+        self.output.print_at(
+            Pos(0, height + 3),
+            self.output.term.darkgray
+            + "\n".join(list(reversed(self.state.messages))[0:5])
+            + self.output.term.normal,
+        )
+
+        # Debug controller state:
+        # for e in state.estore.select(Controller):
+        #     con = e.get(Controller)
+        #     output.print_at(
+        #         Pos(0, height + 3), output.term.blue + repr(con) + output.term.normal
+        #     )
+
+        # bounds
+        hbar = "+" + ("-" * (width - 2)) + "+"
+        bounds = hbar + "\n"
+        for y in range(0, height):
+            bounds += "|" + (" " * (width - 2)) + "|\n"
+        bounds += hbar
+        self.output.print_at(Pos(0, 0), bounds)
+
+        # status
+        # with output.offset(Pos(0, height + 1)):
+        #     item_str = ""
+        #     item = self.last_item_at(state, state.player.pos)
+        #     if item:
+        #         item_str = f" {item.name} "
+        #     obst_str = ""
+        #     obst = self.obstacle_at(state, state.player.pos)
+        #     if obst:
+        #         obst_str = f" >> {obst.name} "
+        #     output.print_at(
+        #         Pos(2, 0),
+        #         f"({state.player.pos.x},{state.player.pos.y}){item_str}{obst_str}",
+        #     )
+
+        # inventory
+        # with output.offset(Pos(0, height + 2)):
+        #     output.print_at(
+        #         Pos(0, 0),
+        #         f"{output.term.normal}Gear: {output.term.gold_on_black}{', '.join([i.name for i in state.player.items])}{output.term.normal}",
+        #     )
